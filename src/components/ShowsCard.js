04b2a5010f3368a356bb347a65484cf69fd235e1
Mali Michael <malimccalla@gmail.com>
2016-11-08 21:41:40 +0000

    start to scaffold card layout

--- src/components/ShowsCard.js
+++ src/components/ShowsCard.js
@@ -5,7 +5,10 @@ import Button from 'react-native-button';
 const ShowsCard = () => {
   return (
     <View style={styles.containerStyle}>
-      <View />
+      <View style={{ backgroundColor: '#fff', height: 60 }}>
+        <View style={styles.dateContainer} />
+      </View>
+      <View style={{ backgroundColor: '#b11', height: 140 }} />
       <Button style={styles.buttonStyle}>Full Details</Button>
     </View>
   );
@@ -27,12 +30,22 @@ const styles = {
     padding: 10,
     backgroundColor: '#0DEBB5',
     borderColor: '#0DEBB5',
-    alignSelf: 'center',
+    alignSelf: 'stretch',
     borderRadius: 2,
     alignItems: 'center',
     fontWeight: '600',
     color: '#fff',
     overflow: 'hidden'
+  },
+  dateContainer: {
+    height: 60,
+    width: 60,
+    borderWidth: 1,
+    borderColor: '#D8D8D8',
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    marginRight: 5,
+    marginTop: 5
   }
 };
 
